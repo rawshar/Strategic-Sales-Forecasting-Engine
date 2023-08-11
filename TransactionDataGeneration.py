d963069eaf8ff494db422f4373aebf94567f97fa
rawshar <34926093+rawshar@users.noreply.github.com>
2023-08-11 12:58:55 -0400

    Create TransactionDataGeneration.py

---
+++ TransactionDataGeneration.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import random
+from datetime import datetime, timedelta
+
+
+num_transactions = 200000
+
+
+quantities = [0.5, 0.25, 1, 0.75, 2, 3, 4, 5, 6, 7] 
+
+
+transaction_data = {
+    'transaction_id': range(1, num_transactions + 1),
+    'customer_id': [random.randint(1, 20001) for _ in range(num_transactions)], 
+    'product_id': [random.randint(1, 20001) for _ in range(num_transactions)], 
+    'quantity': [random.choice(quantities) for _ in range(num_transactions)],
+    'transaction_date': [datetime.now() - timedelta(days=random.randint(0, 365)) for _ in range(num_transactions)]
+}
+
+
+transactions_df = pd.DataFrame(transaction_data)
+
+
+transactions_df.to_parquet('transactions.parquet', index=False)
